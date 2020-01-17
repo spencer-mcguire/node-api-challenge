555183cd776715d9b5c429a06e024cb78badde96
spencer-mcguire <smcguire1826@gmail.com>
2020-01-17 12:12:18 -0600

    add cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 const logger = require("./custom middleware/logger");
 
 const projectRouter = require("./router/projectRouter");
@@ -9,6 +10,7 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 server.use(logger);
 
 server.use("/api/projects", projectRouter);
